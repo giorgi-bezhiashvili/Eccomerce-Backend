42fa2bbe8a06087c52c390eb872d4588765e9522
giorgi-bezhaishvili <giorgibezhiashvili5@gmail.com>
2026-04-10 11:36:44 +0400

    Turned HTTP server to HTTPS

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const app = express();
+const https = require("https");
+const fs = require("fs");
 
 const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
 
 app.use(express.json());
+
 
 app.use("/users", userRoutes);
 app.use("/product", productRoutes);
 
-app.listen(3000, () => console.log(`Server spinning on port 3000`));
+
+
+https.createServer(options, app).listen(3000, () => {
+  console.log(`Secure server spinning on https://localhost:3000`);
+});
