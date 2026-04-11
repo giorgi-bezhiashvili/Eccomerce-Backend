874aeed7e38314fbb47cd6c175f665c542e2d436
giorgi-bezhaishvili <giorgibezhiashvili5@gmail.com>
2026-04-11 12:16:01 +0400

    Made my backend more secure made it https added helmet added jwt stoped brute force attacks

--- index.js
+++ index.js
@@ -2,16 +2,23 @@ const express = require("express");
 const app = express();
 const https = require("https");
 const fs = require("fs");
+const helmet = require("helmet")
+const rateLimit = require(`express-rate-limit`)
 
+const limiter = rateLimit({
+    windowMs : 60 * 1000,
+    max:20,
+    message:"Request limit is reached"
+})
 const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
 const options = {
   key: fs.readFileSync('key.pem'),
   cert: fs.readFileSync('cert.pem')
 };
-
+app.use(helmet())
 app.use(express.json());
-
+app.use(limiter)
 
 app.use("/users", userRoutes);
 app.use("/product", productRoutes);
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 const bcrypt = require("bcrypt");
-
 const DATA_FILE = "./data.json";
 
 const getFileData = () => {
